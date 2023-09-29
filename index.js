3497a43fca70d27811091855c111f763a9fff152
humayra akter <humayraakter25@gmail.com>
2023-09-29 22:42:37 +0600

    api updated

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const bodyParser = require("body-parser");
-const multer = require("multer");
-const path = require("path");
-const { generateUniqueId } = require("./utils");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,98 +21,29 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-// Configure multer for file uploads with size limits
-const upload = multer({
-  limits: {
-    fileSize: 10 * 1024 * 1024, // 10MB (adjust as needed)
-  },
-});
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Specify the folder where uploaded files will be stored
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const ext = path.extname(file.originalname);
-    cb(null, file.fieldname + "-" + uniqueSuffix + ext);
-  },
-});
-
 async function run() {
   try {
     await client.connect();
     const maidCollection = client.db("elite-dwell-assist").collection("maid");
 
     //maid post
-    app.post("/maid", upload.single("image"), async (req, res) => {
-      const {
-        name,
-        email,
-        pass,
-        phone,
-        experience,
-        task,
-        salary,
-        availability,
-        location,
-        nid_no,
-        address,
-        dob,
-        gender,
-        education,
-      } = req.body;
-      const imgData = req.file ? req.file.buffer.toString("base64") : null;
-
-      const newMaid = {
-        maid_id: generateUniqueId(),
-        name,
-        email,
-        pass,
-        phone,
-        img: imgData,
-        experience,
-        task,
-        salary,
-        availability,
-        location,
-        nid_no,
-        address,
-        dob,
-        gender,
-        education,
-      };
-
-      try {
-        const result = await maidCollection.insertOne(newMaid);
-        if (result.insertedCount === 1) {
-          // Successfully inserted, send a success response
-          res.json({ message: "Maid added successfully", data: result.ops[0] });
-        } else {
-          // Failed to insert, send an error response
-          res.status(500).json({ error: "Failed to add maid" });
-        }
-      } catch (error) {
-        console.error("Error inserting maid:", error);
-        res.status(500).json({ error: "Failed to add maid" });
+    app.post("/maid", async (req, res) => {
+      const maid = req.body;
+      const result = await maidCollection.insertOne(maid);
+      if (result.insertedCount === 1) {
+        res.send(result);
+        res.status(201).json({ message: "Maid added successfully" });
+      } else {
+        res.status(500).json({ message: "Failed to add maid" });
       }
     });
 
     //maid get
     app.get("/maid", async (req, res) => {
-      try {
-        const maidCollection = client
-          .db("elite-dwell-assist")
-          .collection("maid");
-
-        // Fetch all maids from the collection
-        const maids = await maidCollection.find({}).toArray();
-
-        res.json(maids);
-      } catch (error) {
-        console.error("Error fetching maids:", error);
-        res.status(500).json({ error: "Failed to fetch maids" });
-      }
+      const query = {};
+      const cursor = maidCollection.find(query);
+      const maids = await cursor.toArray();
+      res.send(maids);
     });
   } finally {
   }
